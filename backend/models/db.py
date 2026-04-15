a4f044bae51d4d5d63bd3730002f24fbc7928d06
Efra <efrainhernandez.2704@gmail.com>
2026-04-15 01:45:11 -0600

    ss
    ss

--- backend/models/db.py
+++ backend/models/db.py
@@ -1,5 +1,11 @@
 from pymongo import MongoClient
+import certifi
 from config import Config
 
-client = MongoClient(Config.MONGO_URI)
+client = MongoClient(
+    Config.MONGO_URI,
+    tls=True,
+    tlsCAFile=certifi.where(),
+    serverSelectionTimeoutMS=30000,
+)
 db = client[Config.DB_NAME]
